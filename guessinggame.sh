bd456c9861e2024903717cf3790337de967d1e26
tounsisabrine <sabrine.tounsi.st@gmail.com>
2020-06-22 17:01:40 +0100

    up to date

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,11 +1,19 @@
-#function getthenumber {
+#!/usr/bin/env bash
+#file : guessinggame.sh
 number=$(ls -F | egrep -v / | wc -l )
-#echo $count
-#}
 
-#number= $(getthenumber)
+#guessing game
+
 echo "guess the number of files in this diroctory: "
 read answer
+#function win
+function win {
+if [[ $answer -eq $number ]]
+then
+echo "congratulations you won"
+fi
+}
+#guessing
 while [[ $answer -ne $number ]]
 do
 if [[ $answer -gt $number ]]
@@ -21,7 +29,4 @@ then
 echo "make sure you entered a number"
 fi
 done
-if [[ $answer -eq $number ]]
-then
-echo "congratulations you won"
-fi
+win
